15f1ee9a53f5d401788563a7cfe586d87ab53a25
ranggakrisnaa <infinity.duos@gmail.com>
2023-12-24 15:12:59 +0700

    add env variabel

--- database/config.go
+++ database/config.go
@@ -2,18 +2,26 @@ package db
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/ranggakrisnaa/task-5-pbi-btpns-rangga/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Init() *gorm.DB {
-	dsn := "host=localhost user=postgres password=postgres dbname=photos_db port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dsn := os.Getenv("DB_URL")
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 	db.AutoMigrate(&models.User{}, &models.Photo{})
 
 	return db
