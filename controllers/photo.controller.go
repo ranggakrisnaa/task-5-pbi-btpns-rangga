8afa745f5aba8fc92db217760aac5091df4f90dc
ranggakrisnaa <infinity.duos@gmail.com>
2023-12-26 11:50:28 +0700

    feature : completed endpoint get and update photo

--- controllers/photo.controller.go
+++ controllers/photo.controller.go
@@ -58,10 +58,76 @@ func UploadPhotoProfile(ctx *gin.Context) {
 }
 
 func GetPhotoProfile(ctx *gin.Context) {
+	userID, _ := ctx.Get("userID")
 
+	db := db.Init()
+	var photos []models.Photo
+
+	result := db.Where("user_id = ?", userID).Preload("User").Find(&photos)
+
+	if result.Error != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"message": "Failed to fetch photos",
+		})
+
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "Successfully retrieve data",
+		"data":    photos,
+	})
 }
 
 func UpdatePhotoProfile(ctx *gin.Context) {
+	photoID := ctx.Param("id")
+	userID, _ := ctx.Get("userID")
+
+	file, err := ctx.FormFile("photo_file")
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Missing photo file"})
+		return
+	}
+
+	timestamp := time.Now().UnixNano()
+	filePhoto := fmt.Sprintf("%d-%s", timestamp, strings.ReplaceAll(file.Filename, " ", ""))
+	filename := "http://localhost:8080/api/v1/photos/" + filePhoto
+
+	updateData := app.PhotoReqBody{
+		Title:    ctx.PostForm("title"),
+		Caption:  ctx.PostForm("caption"),
+		PhotoUrl: filename,
+	}
+
+	photo := models.Photo{
+		Title:    updateData.Title,
+		Caption:  updateData.Caption,
+		PhotoUrl: updateData.PhotoUrl,
+	}
+
+	db := db.Init()
+	result := db.Where("id = ?", photoID).Where("user_id", userID).Preload("User").First(&photo)
+
+	if result.Error != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": "Photo not found",
+			"success": false,
+		})
+		return
+	}
+
+	if err := db.Save(&photo).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "Photo updated successfully",
+		"data":    photo,
+	})
 
 }
 
